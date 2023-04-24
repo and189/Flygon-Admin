023bdfb043cb614eecfd1a2e6c9079fafac43467
and189 <and1_89@live.de>
2023-04-24 20:40:09 +0100

    Update WorkerList.tsx

--- client/src/features/workers/WorkerList.tsx
+++ client/src/features/workers/WorkerList.tsx
@@ -11,6 +11,26 @@ import {
 import { Worker } from './type'
 
 export const WorkerList = () => {
+  const now = new Date();
+  const oneMinuteAgo = new Date(now.getTime() - 60 * 1000);
+
+  const lastSeenInSeconds = (record: Worker) => {
+    const lastSeen = new Date(record.last_seen);
+    const seconds = (now.getTime() - lastSeen.getTime()) / 1000;
+    return `${Math.floor(seconds)} seconds ago`;
+  };
+
+  const averageLastMinute = (record: Worker) => {
+    const lastMinute = record.last_seen > oneMinuteAgo.toISOString();
+    if (!lastMinute) return null;
+    const timeDiffs = record.last_seen_history
+      .filter((dateStr: string) => new Date(dateStr) > oneMinuteAgo)
+      .map((dateStr: string) => now.getTime() - new Date(dateStr).getTime());
+    if (timeDiffs.length === 0) return null;
+    const avgTimeDiff = timeDiffs.reduce((a, b) => a + b, 0) / timeDiffs.length;
+    return `${Math.floor(avgTimeDiff / 1000)} seconds`;
+  };
+
   return (
     <List
       pagination={<Pagination rowsPerPageOptions={[25, 50, 100]} />}
@@ -28,12 +48,22 @@ export const WorkerList = () => {
           label="Location"
           render={(record) => {
             if (!record) return null
-            return `${record.step || 0}/${record.end_step || 0}`
+            const totalLength = (record.end_step || 0) - (record.start_step || 0)
+            const currentPos = (record.step || 0) - (record.start_step || 0)
+            return `${currentPos}/${totalLength}`
           }}
         />
         <TextField source="username" />
         <TextField source="host" />
         <DateField source="last_seen" showDate showTime />
+        <FunctionField<Worker>
+          label="Last seen"
+          render={lastSeenInSeconds}
+        />
+        <FunctionField<Worker>
+          label="Avg last minute"
+          render={averageLastMinute}
+        />
       </Datagrid>
     </List>
   )
