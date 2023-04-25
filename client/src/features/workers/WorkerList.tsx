7add1db68d537f8fe648e9ae4d300d860140b892
and189 <and1_89@live.de>
2023-04-25 17:06:09 +0100

    Update WorkerList.tsx
    used a useRef hook to store the scroll position of the page

--- client/src/features/workers/WorkerList.tsx
+++ client/src/features/workers/WorkerList.tsx
@@ -6,56 +6,58 @@ import {
   Pagination,
   ReferenceField,
   TextField,
+  useRefresh,
 } from 'react-admin'
 
 import { Worker } from './type'
+import { useEffect, useRef } from 'react'
 
 export const WorkerList = () => {
-  const now = new Date();
-  const oneMinuteAgo = new Date(now.getTime() - 60 * 1000);
+  const refresh = useRefresh()
+  const scrollRef = useRef(null)
 
-  const lastSeenInSeconds = (record: Worker) => {
-    const lastSeen = new Date(record.last_seen);
-    const seconds = (now.getTime() - lastSeen.getTime()) / 1000;
-    return `${Math.floor(seconds)} seconds ago`;
-  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      refresh()
+    }, 2000)
+    return () => clearInterval(timer)
+  }, [refresh])
 
-
+  useEffect(() => {
+    if (scrollRef.current) {
+      // Restore scroll position after refresh
+      scrollRef.current.scrollIntoView()
+    }
+  })
 
   return (
-    <List
-      pagination={<Pagination rowsPerPageOptions={[25, 50, 100]} />}
-      title="Workers"
-      actions={false}
-      perPage={25}
-      sort={{ field: 'uuid', order: 'ASC' }}
-    >
-      <Datagrid>
-        <TextField source="uuid" label="UUID" />
-        <ReferenceField source="area_id" reference="areas">
-          <TextField source="name" />
-        </ReferenceField>
-        <FunctionField<Worker>
-          label="Location"
-          render={(record) => {
-            if (!record) return null
-            const totalLength = (record.end_step || 0) - (record.start_step || 0)
-            const currentPos = (record.step || 0) - (record.start_step || 0)
-            return `${currentPos}/${totalLength}`
-          }}
-        />
-        <TextField source="username" />
-        <TextField source="host" />
-        <DateField source="last_seen" showDate showTime />
-        <FunctionField<Worker>
-          label="Last seen"
-          render={lastSeenInSeconds}
-        />
-        <FunctionField<Worker>
-          label="Avg last minute"
-          render={averageLastMinute}
-        />
-      </Datagrid>
-    </List>
+    <div ref={scrollRef}>
+      <List
+        pagination={<Pagination rowsPerPageOptions={[25, 50, 100]} />}
+        title="Workers"
+        actions={false}
+        perPage={25}
+        sort={{ field: 'uuid', order: 'ASC' }}
+      >
+        <Datagrid>
+          <TextField source="uuid" label="UUID" />
+          <ReferenceField source="area_id" reference="areas">
+            <TextField source="name" />
+          </ReferenceField>
+          <FunctionField<Worker>
+            label="Location"
+            render={(record) => {
+              if (!record) return null
+              const totalLength = (record.end_step || 0) - (record.start_step || 0)
+              const currentPos = (record.step || 0) - (record.start_step || 0)
+              return `${currentPos}/${totalLength}`
+            }}
+          />
+          <TextField source="username" />
+          <TextField source="host" />
+          <DateField source="last_seen" showDate showTime />
+        </Datagrid>
+      </List>
+    </div>
   )
 }
